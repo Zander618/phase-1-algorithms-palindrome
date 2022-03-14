89a3256fb46fdca33271057fcd1b5888eb9ca030
Zachary Sirna <zsirna@yahoo.com>
2022-03-13 21:05:17 -0400

    commit tree 6ad767f70fbb39e63e12879957ac6708fcbc8b8d

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  if (word === reverseString(word)){
+    return true
+  }else{
+    return false
+  }
 }
 
 /* 
-  Add your pseudocode here
+  must learn to reverse a string
 */
 
 /*
-  Add written explanation of your solution here
+  create a function that reverses a string and then use that function in the isPalindrome function
 */
+
+function reverseString(string){
+  let splitIt = string.split("")
+  let reverseIt = splitIt.reverse()
+  let joinIt = reverseIt.join("")
+  return joinIt
+}
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  console.log(reverseString("hello"))
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
